7623b1872abb3c413443aa91d6e6f0e96d767f98
DealiAxy <dealiaxy@gmail.com>
2018-10-28 22:29:17 +0800

    夜间模式还没做完呢，这个回宿舍再继续写了~~
    Signed-off-by: DealiAxy <dealiaxy@gmail.com>

--- app/src/main/java/cn/deali/minimalpoem/MainApp.java
+++ app/src/main/java/cn/deali/minimalpoem/MainApp.java
@@ -2,6 +2,8 @@ package cn.deali.minimalpoem;
 
 import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.app.AppCompatDialog;
 
 import cn.deali.minimalpoem.db.DaoMaster;
 import cn.deali.minimalpoem.db.DaoSession;
@@ -22,9 +24,35 @@ public class MainApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // 自动开启夜间模式
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
         mApp = this;
         config = new ConfigManager(this);
         daoSession = initGreenDao();
+    }
+
+
+    public void setNightMode(boolean isEnabled) {
+        if (isEnabled)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    public boolean getNightMode() {
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        switch (nightMode) {
+            case AppCompatDelegate.MODE_NIGHT_YES:
+                return true;
+            case AppCompatDelegate.MODE_NIGHT_NO:
+                return false;
+            case AppCompatDelegate.MODE_NIGHT_AUTO:
+                return false;
+            case AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM:
+                return false;
+            default:
+                return false;
+        }
     }
 
 
